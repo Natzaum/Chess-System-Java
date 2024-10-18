6e5b436efe59bb735442d5f0af27c31d7fd1bed0
Natzaum <natacezerbordignon@gmail.com>
2024-10-18 14:39:48 -0300

    Start to implement board piece

--- src/Aplicattion/Program.java
+++ src/Aplicattion/Program.java
@@ -1,10 +1,10 @@
 package Aplicattion;
 
+import BoardGame.Board;
 import BoardGame.Position;
 
 public class Program {
     public static void main (String[] args){
-        Position pos = new Position(5, 3);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
